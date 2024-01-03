664e1423c1220d51f7b1c983191562aa1b0fa34d
saranedhal <sarahnedhal@gmail.com>
2024-01-03 14:37:52 +0300

    fixed category and service functionality

--- src/components/Category/CategoryList.jsx
+++ src/components/Category/CategoryList.jsx
@@ -45,17 +45,27 @@ export default function CategoryList(props) {
  
 
     const addCategory = (category) =>{
-        let newCategory = {
-            ...category, 
-             userId: props.user.id
-        }
-        const formData = new FormData()
-        formData.append("addCategory",JSON.stringify(newCategory))
-        formData.append("image", file)
-        Axios.post("/category/add",formData , { headers: {'Content-Type': 'multipart/form-data'}})
+        // const formData = new FormData()
+
+        // let newCategory = {
+        //     ...category, 
+        //      userId: props.user.id
+        // }
+        // console.log("category" , category);
+        // console.log("user id" , props.user.id);
+        // console.log("the category info " , newCategory);
+        // console.log("JSON.stringify(newCategory) " , JSON.stringify(newCategory));
+        // let data = JSON.stringify(newCategory);
+        // formData.append("addCategory", data)
+        // console.log("form data for category" , formData);
+        // formData.append("image", file)
+        // console.log("form data for image" , formData);
+        // console.log("the form data" , formData);
+        Axios.post("/category/add",category , setHeader() , { headers: {'Content-Type': 'multipart/form-data'}})
            .then(res =>{
             console.log("Category Added successfuly !!");
             console.log(res);
+            loadCategoryList();
            })
            .catch(err =>{
             console.log("Error adding Category :" , err);
@@ -94,6 +104,7 @@ export default function CategoryList(props) {
                 console.log(res);
                 loadCategoryList();
                 setIsEdit(false);
+                loadCategoryList();
             })
             .catch(error=>{
                 console.log("error in updating category");
@@ -143,12 +154,11 @@ export default function CategoryList(props) {
     {categoryList}
     </div>
 
-    {/* {(!isEdit) ? */}
+    {(!isEdit) ?
     <CategoryCreateForm addCategory={addCategory} setFile={setFile} user={props.user}/>
-     {/* : */}
+     :
     <CategoryEditForm key={currentCategory._id} category={currentCategory} updatedCategory={updatedCategory} setFile={setFile} user={props.user}/>
-
-    {/* } */}
+    }
      </div>
 
     </>
