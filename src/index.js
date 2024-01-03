bf53b1eb25d6a79736bca399494125b3572ac406
yousif <yousif.dhneem@gmail.com>
2024-01-03 20:00:16 +0300

    moved the nav bar with the design

--- src/index.js
+++ src/index.js
@@ -8,28 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <div >
-        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content--between py-9 mb-7 border-bottom bg-warning">
-            <div className="col-md-3 mb-2 mb-md-0">
-                <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-                    <img src='../maintence5.png' alt="logo" width="90" height="70"  />
-                </a>
-            </div>
-
-            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/" className="nav-link px-2 link-secondary">Home</a></li>
-                <li><a href="category/index" className="nav-link px-2  link-dark">Category</a></li>
-        
-            </ul>
-
-            <div className="col-md-3 text-end">
-              <button type="button" className="btn btn-dark"><a href="/signin" className="nav-link px-2  link-light">Login</a></button> &nbsp;
-
-                <button type="button" className="btn btn-dark"><a href="/signup" className="nav-link px-2  link-light">SignUp</a></button>
-            </div>
-        </header>
-        
-    </div>
+   
     <App />
     
     <div className="b-example-divider">
